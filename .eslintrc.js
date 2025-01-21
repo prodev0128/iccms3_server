568c9b93edba3e137ec35b61e96c5c7d6c1a3d01
prodev0128 <prodev.development0128@gmail.com>
2025-01-21 07:41:26 -0500

    feat(env): change config of import arrangement order

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,14 +31,8 @@ module.exports = {
       'error',
       {
         groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-          'type',
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index', 'object', 'type'],
         ],
         alphabetize: {
           order: 'asc',
