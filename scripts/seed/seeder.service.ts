cf80066baac84e49f2652b5ee9feba4ae2eac6c8
prodev0128 <prodev.development0128@gmail.com>
2025-05-08 18:35:18 -0400

    feat(seed): make seed script

--- scripts/seed/seeder.service.ts
+++ scripts/seed/seeder.service.ts
@@ -2,14 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import fs from 'fs-extra';
 import { Model } from 'mongoose';
-import { Setting, SettingDocument } from '@app/database';
+import path from 'path';
+import { Code, CodeDocument, Setting, SettingDocument, User, UserDocument } from '@app/database';
 
 @Injectable()
 export class SeederService {
-  constructor(@InjectModel(Setting.name) private settingModel: Model<SettingDocument>) {}
+  constructor(
+    @InjectModel(Setting.name) private settingModel: Model<SettingDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Code.name) private codeModel: Model<CodeDocument>,
+  ) {}
 
   async seed() {
-    const seedData = JSON.parse(fs.readFileSync('./seed-data.json', 'utf8'));
-    console.log('seedData', seedData);
+    try {
+      const filePath = path.resolve(__dirname, 'seed-data.json');
+      const seedData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+      const findAnyOne = await this.codeModel.findOne();
+      if (findAnyOne) {
+        console.log('Data already exists in the database.');
+        return;
+      }
+      // await this.userModel.insertMany(seedData.users);
+      // await this.settingModel.insertMany(seedData.settings);
+      await this.codeModel.insertMany(seedData.codes);
+    } catch (e) {
+      console.log('e', e);
+    }
   }
 }
